e0c42780a8470772d422001ebf6b998c03deb4f0
Vitalij Ryndin <sys@crashmax.ru>
2021-12-05 19:52:36 +0800

    1.0.3

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
